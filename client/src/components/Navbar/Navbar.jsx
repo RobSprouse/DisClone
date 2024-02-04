036fca7ce7fc1ffb5639d52e9b6fdfd0c186c079
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2024-02-04 13:18:52 -0500

    logout works

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
 import { useContext, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
+import { useMutation } from "@apollo/client";
 import AccessTokenContext from "../../utils/AccessTokenContext";
+import { LOGOUT_USER } from "../../utils/mutations";
 import "./Navbar.css";
 
 function Navbar() {
      const { accessToken, setAccessToken } = useContext(AccessTokenContext);
+     const [logout] = useMutation(LOGOUT_USER);
 
-     const handleLogout = useCallback(() => {
-          setAccessToken(null);
-     }, [setAccessToken]);
+     const handleLogout = useCallback(async () => {
+          try {
+               await logout();
+               setAccessToken(null);
+          } catch (error) {
+               console.error("Failed to logout:", error);
+          }
+     }, [logout, setAccessToken]);
 
      return (
           <nav>
