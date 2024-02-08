9e3874685958a5341bda49976731195735359949
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2024-02-07 20:38:21 -0500

    link to messages

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -3,12 +3,14 @@ import { useLocation } from "react-router-dom";
 const Messages = () => {
      const location = useLocation();
      const { id } = location.state;
+     const { type } = location.state;
      return (
           <div>
                <h1>Messages</h1>
                <h2>{id}</h2>
+               <h2>{type}</h2>
           </div>
      );
 };
 
-export default Messages
+export default Messages;
