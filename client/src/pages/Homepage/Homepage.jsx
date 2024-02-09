bd72de8f08ad87aaa47c874af3578958d8a015fc
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2024-02-08 21:20:33 -0500

    edited homepage
    added card for sidebar, hid member ids and names

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,6 +1,34 @@
 import { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_USER } from "../../utils/queries";
+import {
+     Card,
+     Typography,
+     List,
+     ListItem,
+     ListItemPrefix,
+     ListItemSuffix,
+     Chip,
+     Accordion,
+     AccordionHeader,
+     AccordionBody,
+     Alert,
+     Avatar,
+   } from "@material-tailwind/react";
+   import {
+     PresentationChartBarIcon,
+     ShoppingBagIcon,
+     UserCircleIcon,
+     Cog6ToothIcon,
+     InboxIcon,
+     PowerIcon,
+   } from "@heroicons/react/24/solid";
+   import {
+     ChevronRightIcon,
+     ChevronDownIcon,
+     CubeTransparentIcon,
+     MagnifyingGlassIcon,
+   } from "@heroicons/react/24/outline";
 
 const Homepage = () => {
      const [user, setUser] = useState(null);
@@ -28,26 +56,27 @@ const Homepage = () => {
      };
 
      return (
+          <Card className="h-full w-full max-w-[12rem] p-4 shadow-xl shadow-blue-gray-900/5 bg-gray-100">
           <div>
                {user && (
                     <>
-                         <h1>Welcome {user.username}</h1>
-                         <h2>Channels</h2>
+                         <Typography varient="h5" color="blue-gray " className="font-bold text-xl">Welcome! {user.username}</Typography>
+                         <Typography varient="h5" color="blue-gray " className="font-bold text-xl">Channels</Typography>
                          {user.channels.map((channel) => (
-                              <div key={channel._id}>
+                              <div  key={channel._id}>
                                    <p className={style.channelName}>{channel.name}</p>
                                    <img className={style.channelImage} src={channel.image} alt={channel.name} />
                               </div>
                          ))}
-                         <h2>Conversations</h2>
+                         <Typography varient="h5" color="blue-gray " className="font-bold text-xl mt-6">Conversations</Typography>
                          {user.conversations.map((conversation) => (
                               <div key={conversation._id}>
-                                   <h3>Conversation ID: {conversation._id}</h3>
+                                   <h3 className="invisible">{conversation._id}</h3>
                                    {conversation.members
                                         .filter((member) => member._id !== user._id)
                                         .map((member) => (
                                              <div key={member._id}>
-                                                  <h4>{member.username}</h4>
+                                                  <h4 className="invisible">{member.username}</h4>
                                                   <img
                                                        className={style.memberImage}
                                                        src={member.image}
@@ -60,7 +89,9 @@ const Homepage = () => {
                     </>
                )}
           </div>
+         </Card> 
      );
 };
+
 
 export default Homepage;
