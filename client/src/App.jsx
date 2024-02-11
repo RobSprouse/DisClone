7d81781212a816c6236bd11d943606cdbddcbcdc
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2024-02-11 17:42:18 -0500

    sign up form and login in integrated differently so we can set up the outlet right

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Cookies from "js-cookie";
 import { createApolloClient } from "./utils/apolloClient";
@@ -7,17 +7,16 @@ import { ThemeProvider } from "@material-tailwind/react";
 import AccessTokenContext from "./utils/AccessTokenContext";
 import NavigationBar from "./components/Navbar/Navbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import LoginForm from "./components/LoginForm/LoginForm.jsx";
-import SignUpForm from "./components/SignupForm/SignupForm.jsx";
+
 import "./app.css";
 
 function App() {
      const [accessToken, setAccessToken] = useState(null);
-     const [showSignUp, setShowSignUp] = useState(false);
      const [isLoading, setIsLoading] = useState(true);
      const location = useLocation();
 
      const client = useMemo(() => createApolloClient(accessToken), [accessToken]);
+     const contextValue = useMemo(() => ({ accessToken, setAccessToken }), [accessToken, setAccessToken]);
 
      const refreshAccessToken = async () => {
           try {
@@ -49,27 +48,16 @@ function App() {
           setIsLoading(false); // Set isLoading to false after reading the token
      }, [location]);
 
-     const toggleShowSignUp = useCallback(() => {
-          setShowSignUp((prevShowSignUp) => !prevShowSignUp);
-     }, []);
-
      // If isLoading is true, render a loading message or spinner
      if (isLoading) return <></>;
 
      return (
           <ApolloProvider client={client}>
-               <AccessTokenContext.Provider value={{ accessToken, setAccessToken }}>
+               <AccessTokenContext.Provider value={contextValue}>
                     <React.StrictMode>
                          <ThemeProvider>
                               <NavigationBar />
-                              <main>
-                                   {accessToken ? <Outlet /> : showSignUp ? <SignUpForm /> : <LoginForm />}
-                                   {!accessToken && (
-                                        <button onClick={toggleShowSignUp}>
-                                             {showSignUp ? "Go to Login" : "Go to Sign Up"}
-                                        </button>
-                                   )}
-                              </main>
+                              <Outlet />
                               <Footer />
                          </ThemeProvider>
                     </React.StrictMode>
